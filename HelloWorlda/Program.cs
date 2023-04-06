ff5e8cd109fbafe3955b53c6d9b354e0dc9e2949
kissikek <ermolenkokonstantin1612@gmail.com>
2023-04-06 22:39:53 +0300

    added new tasks))

--- HelloWorlda/Program.cs
+++ HelloWorlda/Program.cs
@@ -1,8 +1,118 @@
-﻿using System.Net.Http.Headers;
-
-namespace HelloWorld;
+﻿namespace HelloWorld;
 class Tasks
 {
+    public static int multiplicityBy17(int c)
+    {
+        while(c % 17 != 0)
+        {
+            c++;
+        }
+        Console.WriteLine(c);
+        return c;
+    }
+    
+    public static void multiplicityByC(int a, int b, int c)
+    {
+        for(int _i = a; _i <= b; _i++)
+        {
+            if(_i % c == 0)Console.WriteLine(_i);
+        }
+    }
+    
+    public static void sumMultiplicityBy4(int a, int b)
+    {
+        int _sum = 0;
+        for(int _i = a; _i <= b; _i++)
+        {
+            if(_i % 4 == 0) _sum+= _i;
+        }
+        Console.WriteLine(_sum);
+    }
+    
+    public static void interval()
+    {
+        int key = Convert.ToInt32(Console.ReadLine());
+        if ((key > -5) & (key < 3)) Console.WriteLine(true);
+        else Console.WriteLine(false);
+    }
+    
+    public static void dayOfWeek()
+    {
+        int key = Convert.ToInt32(Console.ReadLine());
+        switch (key)
+        {
+            case 1:
+            Console.WriteLine("Monday");
+            break;
+            case 2:
+            Console.WriteLine("Tuesday");
+            break;
+            case 3:
+            Console.WriteLine("Wednesday");
+            break;
+            case 4:
+            Console.WriteLine("Thursday");
+            break;
+            case 5:
+            Console.WriteLine("Friday");
+            break;
+            case 6:
+            Console.WriteLine("Saturday");
+            break;
+            case 7:
+            Console.WriteLine("Sunday");
+            break;
+            default: 
+            Console.WriteLine("Chislo doljno bit ot 1 do 7");
+            break;
+        }
+    }
+    
+    public static void amountOfUnits(int a)
+    {
+        Console.WriteLine(a % 10);
+    }
+    
+    public static void amountOfTens(int a)
+    {
+        Console.WriteLine((a / 10) % 10);
+    }
+    
+    public static void sumOfDigits(int a)
+    {
+        int _sum = (a / 100) + ((a / 10) % 10) + (a % 10);  
+        Console.WriteLine(_sum);
+    }
+    
+    public static void productOfDigits(int a)
+    {
+        int _product = (a / 100) * ((a / 10) % 10) * (a % 10);  
+        Console.WriteLine(_product);
+    }
+    
+    public static void revRead(int a)
+    {
+        int _rev = (a / 100) + (((a / 10) % 10) * 10) + ((a % 10) * 100);  
+        Console.WriteLine(_rev);
+    }
+    
+    public static void firstNSecondSwap(int a)
+    {
+        int _swap = ((a / 100) * 10) + (((a / 10) % 10) * 100) + (a % 10) ;  
+        Console.WriteLine(_swap);
+    }
+    
+    public static void allNumbersTheSame(int a)
+    {
+        bool _theSame = ((a / 100) == ((a / 10) % 10)) & ((a / 100) == (a % 10)) ? (true) : (false);
+        Console.WriteLine(_theSame);
+    }
+    
+    public static void anyNumbersTheSame(int a)
+    {
+        if(((a / 100) == ((a / 10) % 10)) | (((a / 10) % 10) == (a % 10)) | ((a % 10) == (a / 100))) Console.WriteLine(true);
+        else Console.WriteLine(false);
+    }
     public static void task1() 
     {
         var str = Console.ReadLine();
